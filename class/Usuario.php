89b6b6e131e5c963b57fb0fb5637fdd6236784b8
Iagoo02 <iagomoises2013@gmail.com>
2022-07-24 17:32:39 -0300

    atualizando a classe usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -37,6 +37,17 @@
             $this->dtcadastro = $dtcadastro;
         }
 
+
+
+
+
+
+        public static function todosUsuarios(){
+            $banco = new Sql();
+
+            return $results = $banco->select("SELECT * FROM tb_usuarios");
+        }
+
         public function loadById($id){
             $banco = new Sql();
             
@@ -54,6 +65,38 @@
 
         }
 
+        public function login($usuario,$password){
+            $banco = new Sql();
+            
+            $results = $banco->select("SELECT * FROM tb_usuarios WHERE deslogin = :USUARIO AND dessenha = :SENHA", array(
+                ":USUARIO"=>$usuario,
+                ":SENHA"=>$password
+            ));
+
+            if(count($results)>0){
+                $row = $results[0];
+                $this->setIdusuario($row['idusuario']);
+                $this->setDeslogin($row['deslogin']);
+                $this->setDessenha($row['dessenha']);
+                $this->setDtcadastro($row['dtcadastro']);
+            }else{
+                throw new Exception("Login e/ou senha errados");
+                
+            }
+
+        }
+
+        public static function search($usuario){
+            $banco = new Sql();
+            
+            $usuariopesq = "%".$usuario."%";
+            return $results = $banco->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :USUARIO", array(
+                ":USUARIO"=>$usuariopesq,
+            ));
+
+        }
+        
+        
         public function __toString(){
             return "idusuario = ".$this->getIdusuario()." deslogin = ".$this->getDeslogin()." dessenha = ".$this->getDessenha()." dtcadastro = ".$this->getDtcadastro(); 
         }
--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
     require_once("config.php");
 
-    $pessoa = new Usuario();
+    //Carregando um usuario pelo id.
+    //$pessoa = new Usuario();
+    //$pessoa->loadById(5);
+    //echo $pessoa;
 
-    $pessoa->loadById(5);
+    //Buscando todos os usuarios
+    //$todos = Usuario::todosUsuarios();
+    //echo json_encode($todos);
 
-    echo $pessoa;
+    //Tentando fazer login
+    //$pessoa = new Usuario();
+    //$pessoa->login("jose","arroz");
+    //echo $pessoa;
+
+    //Procurando usuario por nome
+    $todos = Usuario::search("oo");
+    echo json_encode($todos);
 ?>
